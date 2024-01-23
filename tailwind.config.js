8e74854514be95286df4659137d080046a08e94e
Mountain-PNS <sonpn30176@gmail.com>
2024-01-23 21:38:55 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://image.dienthoaivui.com.vn/x,webp,q100/https://dashboard.dienthoaivui.com.vn/uploads/wp-content/uploads/2022/08/thumb-4.jpg')",
+        
+      }
+    }
+  },
+  plugins: [],
+}
